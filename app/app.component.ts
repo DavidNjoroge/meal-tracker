15c8f56ffa03b7986c18947d893df6b87fa9357a
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-15 12:39:44 +0300

    add the dependencies and angular  framework

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+  <div class="jumbotron">
+  <h1>meal tracker</h1>
+  </div>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
